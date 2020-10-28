b7b70d6f5e3044c068228b9944c130060655c833
Felizardo <devfelizardo@gmail.com>
2020-10-28 02:01:58 -0300

    using request body, query params and request body with insomnia

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+    // Query Params
+    // without destructuring 
+    // const query = request.query;
+    // console.log(query); //output example: { title: 'React', owner: 'Diego' }
+    const {title, owner} = request.query;
+    console.log(title);
+    console.log(owner);
+    
     return response.json([
         'Projeto 1',    
         'Projeto 2'
@@ -10,6 +20,15 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+    // Request Body
+    // Not destructured
+    //const body = request.body;
+    //console.log(body); //output example: { title: 'Aplicativo React', owner: 'Luiz Felizardo' }
+    const {title, owner} = request.body;
+    // REMINDER: return undefined, if not set to JSON with app.use(express.json());.
+    console.log(title);
+    console.log(owner);
+    
     return response.json([
         'Projeto 1',    
         'Projeto 2',
@@ -18,6 +37,13 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+    // Route Params
+    // No destructuring 
+    // const params = request.params;
+    // console.log(params); //output example: { id: '1' }
+    const {id} = request.params;
+    console.log(id);
+    
     return response.json([
         'Projeto 4',    
         'Projeto 2',
